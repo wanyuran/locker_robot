24e747927f9aa89b912c027bf8fefd0b072cfd0c
wanyuran <wan_ivy@163.com>
2020-04-08 16:29:36 +0800

    [Wan Yuran] test: throw exception when lockers are full

--- src/test/java/com/xpbootcamp/locker_robot/SmartRobotTest.java
+++ src/test/java/com/xpbootcamp/locker_robot/SmartRobotTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SmartRobotTest {
 	@Test
@@ -58,5 +57,22 @@ class SmartRobotTest {
 		assertNotNull(ticket);
 		Bag foundBag = locker.getBag(ticket);
 		assertEquals(bag, foundBag);
+	}
+
+	@Test
+	void should_return_error_message_when_save_bag_given_a_robot_and_a_bag_and_full_lockers() {
+		// Given
+		Locker locker = new Locker(0);
+		Locker anotherLocker = new Locker(0);
+
+		Robot smartRobot = new Robot(Arrays.asList(locker, anotherLocker));
+		Bag bag = new Bag();
+
+		// When
+		RuntimeException runtimeException = assertThrows(RuntimeException.class,
+				() -> smartRobot.saveBag(bag));
+
+		// Then
+		assertEquals("当前柜子已满", runtimeException.getMessage());
 	}
 }
